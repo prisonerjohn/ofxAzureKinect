bee13156a50421ae60a47a14a4286b775f0cd335
Elie Zananiri <prisonerjohn@gmail.com>
2019-08-30 17:13:24 -0400

    Move type to Types file.

--- src/ofxAzureKinect/Device.h
+++ src/ofxAzureKinect/Device.h
@@ -11,13 +11,10 @@
 #include "ofVboMesh.h"
 #include "ofVectorMath.h"
 
+#include "Types.h"
+
 namespace ofxAzureKinect
 {
-	typedef k4a_depth_mode_t DepthMode;
-	typedef k4a_color_resolution_t ColorResolution;
-	typedef k4a_image_format_t ImageFormat;
-	typedef k4a_fps_t FramesPerSecond;
-
 	struct DeviceSettings
 	{
 		int deviceIndex;
---
+++ src/ofxAzureKinect/Types.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <k4a/k4atypes.h>
+
+namespace ofxAzureKinect
+{
+	typedef k4a_depth_mode_t DepthMode;
+	typedef k4a_color_resolution_t ColorResolution;
+	typedef k4a_image_format_t ImageFormat;
+	typedef k4a_fps_t FramesPerSecond;
+}
