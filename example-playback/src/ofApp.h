c370ce7de75090c60efe02ed6dbfe34f7096e26a
madelinegannon <mgannon@nvidia.com>
2020-06-23 22:15:49 -0400

    added playback example

---
+++ example-playback/src/ofApp.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "ofMain.h"
+
+#include "ofxAzureKinect.h"
+#include "ofxAzureKinect/Device.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+		void exit();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		ofxAzureKinect::Device device;
+
+		// ofxAzureKinect::Playback playback;
+		string filename;
+		
+		bool play = false;
+		float play_head = 0;
+};
